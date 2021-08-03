6510a5dae4913c0475d1147b6db893dff5856ca9
Yussuf101 <32193666+Yussuf101@users.noreply.github.com>
2021-08-03 11:10:48 +0100

    first update

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,13 +1,28 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h1>Hi, there.</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: []
+  }
+
+  handleFetch = async () => {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await response.json();
+    this.setState({ data: data })
+    // fetch("https://api.chucknorris.io/jokes/random")
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+  };
+
+  render() {
+    const { data } = this.state
+    return (
+      <div>
+        <h1>{data.value && data.value}</h1>
+        <button onClick={this.handleFetch}>click me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
